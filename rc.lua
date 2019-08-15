a4e1e41893e77e4cab629215b127ab4cc2dc5e72
Andrew Radev <andrey.radev@gmail.com>
2019-08-15 20:23:21 +0300

    Don't honor size hints
    Otherwise terminal leaves an annoying gap.

--- rc.lua
+++ rc.lua
@@ -454,14 +454,15 @@ awful.rules.rules = {
   {
     rule       = { },
     properties = {
-      border_width = beautiful.border_width,
-      border_color = beautiful.border_normal,
-      focus        = awful.client.focus.filter,
-      raise        = true,
-      keys         = client_keys,
-      buttons      = client_buttons,
-      screen       = awful.screen.preferred,
-      placement    = awful.placement.no_overlap+awful.placement.no_offscreen
+      border_width     = beautiful.border_width,
+      border_color     = beautiful.border_normal,
+      focus            = awful.client.focus.filter,
+      size_hints_honor = false,
+      raise            = true,
+      keys             = client_keys,
+      buttons          = client_buttons,
+      screen           = awful.screen.preferred,
+      placement        = awful.placement.no_overlap+awful.placement.no_offscreen
     }
   },
 
